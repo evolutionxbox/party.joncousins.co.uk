6be052b743a97d61af0c35221ce5fa2f0117013d
Jonathan Cousins <evolutionxbox@gmail.com>
2022-05-09 08:19:39 +0100

    Update css/html according to lightouse recommendations

--- index.mjs
+++ index.mjs
@@ -2,7 +2,6 @@ const btns = document.querySelectorAll('.get-scrolling');
 
 function onClick (e) {
   e.preventDefault();
-  console.log('hi')
 
   const nextSection = this.closest('.article__section').nextElementSibling;
 
